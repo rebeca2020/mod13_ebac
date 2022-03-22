982d62976246ca108e483aa6d6d88bde0aae4504
rebeca2020 <61558667+rebeca2020@users.noreply.github.com>
2022-03-21 22:26:33 -0300

    Moduo13- ebac
    MOD13:
    -UM POUCO DE 'ORIENTAÇÃO A OBJETOS' NA PRÁTICA;
    INTERFACES
    CLASSES CONCRETAS
    CLASSES ABSTRATAS
    AGREGAÇÃO E COMPOSICAO...

---
+++ Pessoa.java
@@ -0,0 +1,35 @@
+package br.com.exercicio;
+
+public abstract class Pessoa {
+
+	private String nome;
+	private long telefone; 
+	private String endereco;
+	
+
+	
+	public long getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(long telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+}
---
+++ Programa_exercicio.java
@@ -0,0 +1,41 @@
+package br.com.exercicio;
+
+public class Programa_exercicio {
+
+	public static void main(String[] args) {
+		
+		PessoaFisica pf = new PessoaFisica();
+		
+		pf.setNome("Marleci");
+		pf.setCpf("123.152.146-32");
+		pf.setRg("16.958.093-3");
+		pf.setTelefone(22252115l);
+		pf.setEndereco("Estrada das rosas, 748");
+		imprimir(pf);
+		
+		PessoaJuridica pj = new PessoaJuridica();
+		
+		pj.setNome("Bruno");
+		pj.setTelefone(24554421l);
+		pj.setCnpj("31.336.323-7");
+		pj.setEndereco("Rua joão santos, 45");
+		imprimir(pj);
+		
+	
+	}
+
+	private static void imprimir(Pessoa pessoa) {
+		
+		if(pessoa instanceof Pessoa ) {
+			Pessoa pesFis = (Pessoa) pessoa; //faz o casting
+			System.out.println("Telefone do(a) " + pessoa.getNome() + " é " + pessoa.getTelefone()  + " e o endereço é: " + pessoa.getEndereco());
+		}
+	
+	}
+	
+	
+	
+
+}
+
+
